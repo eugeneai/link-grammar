4638bfe8ec353a5212fe428cd3ef83393a1959aa
Evgeny Cherkashin <eugeneai@irnok.net>
2016-08-26 11:38:34 +0800

    Removed --help.

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-./configure --prefix=$PWD/.built --help \
+./autogen.sh
+make distclean
+./configure --prefix=$PWD \
     --enable-debug \
     --enable-pthreads \
     --disable-java-bindings \
-    --enable-viterbi \
-    --enable-corpus-stats \
+#    --enable-viterbi \
+#    --enable-corpus-stats \
     --enable-wordgraph-display \
     --disable-hunspell \
     --disable-editline \
     --enable-regex-tokenizer
 
 make -j 5
-make install
+# make install
